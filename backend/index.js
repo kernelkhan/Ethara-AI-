05ce225a60eb430e20d24625c14462ca8fec4aa7
Kernelkhan <talmeezahmadkhan1122@gmail.com>
2026-04-30 16:55:36 +0530

    Use Regex for catch-all route to fix Express 5 crash

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get('/:any*', (req, res) =>
+  app.get(/^(?!\/api).+/, (req, res) =>
     res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'))
   );
 } else {
